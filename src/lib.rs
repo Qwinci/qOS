272c0f6976ebd9b52ff10c9f906982eefa900d7b
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-01-02 14:49:53 +0200

    Added support for :upper

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,25 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn replace(items: TokenStream) -> TokenStream {
-    let mut items_str = items.to_string();
+	let mut items_str = items.to_string();
 
-    let matches: Vec<_> = items_str.match_indices("[<").map(|(index, _)| index)
-        .zip(items_str.match_indices(">]").map(|(index, _)| index)).collect();
+	while let (Some(start), Some(end)) = (items_str.find("[<"), items_str.find(">]")) {
+		let mut replace = items_str[start + 2..end].to_string();
 
-    for (start, end) in matches {
-        let mut replace = items_str[start + 2..end].to_string();
-        replace.retain(|c| !c.is_whitespace());
-        items_str.replace_range(start..end + 2, &replace);
-    }
+		while let Some(upper_start) = replace.find(" : upper") {
+			let before_upper = &replace[..upper_start];
+			let word_start = before_upper.rfind(|c: char| c.is_whitespace())
+				.map(|i| i + 1).unwrap_or(0);
+			let word = &before_upper[word_start..];
+			let upper_word = word.to_uppercase();
 
-    items_str.parse().unwrap()
+			replace.replace_range(word_start..upper_start + 8, &upper_word);
+		}
+
+		replace.retain(|c| !c.is_whitespace());
+
+		items_str.replace_range(start..end + 2, &replace);
+	}
+
+	items_str.parse().unwrap()
 }
